94dc4ced72aab2f8dba173fe6e49662bd7db8867
Laurens <contac@lphosting.nl>
2019-04-11 08:30:03 -0400

    Added variables for the view and setup for the Speech model

--- Speech2Text/ViewController.swift
+++ Speech2Text/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var fadedView: UIView!
+    @IBOutlet weak var recordingView: UIView!
+    @IBOutlet weak var recordedMessage: UITextView!
+    
+    var speechData: [Speech]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
