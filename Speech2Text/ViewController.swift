eebbcb7e5becfe712d3619ef7144a195c3d734ff
Jurjen <jurjen@jurjens-mbp.access.nhl.nl>
2019-04-11 12:59:04 +0200

    Initial Commit

---
+++ Speech2Text/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Spraak naar tekst
+//
+//  Created by Laurens Post
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
