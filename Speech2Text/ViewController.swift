a8955d240243e57672ac336fc5dad7cd10343ee5
Jurjen Borkent <jurjenborkent@gmail.com>
2019-04-16 14:28:20 +0200

    Added a print to function

--- Speech2Text/ViewController.swift
+++ Speech2Text/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        print("hello");
     }
 }
 
