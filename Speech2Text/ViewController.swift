0cf07998f8c0a5fc721bb1594a7cc7475ed1d3b4
Laurens <contac@lphosting.nl>
2019-04-16 08:31:34 -0400

    Added speechrequest, connected the buttons for stop and start recording and set the recordingformat for the speech

--- Speech2Text/ViewController.swift
+++ Speech2Text/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
     
@@ -49,6 +50,114 @@ class ViewController: UIViewController {
         // hide recording views
         self.recordingView.isHidden = true
         self.fadedView.isHidden = true
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func requestAuth() {
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            DispatchQueue.main.async {
+                switch authStatus {
+                case .authorized:
+                    self.recordButton.isEnabled = true
+                case .denied, .notDetermined, .restricted:
+                    self.recordButton.isEnabled = false
+                    
+                }
+            }
+        }
+    }
+    
+    @IBAction func didTapRecordButton(sender: UIButton) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+        } else {
+            self.startRecording()
+            self.recordingView.isHidden = false
+            self.fadedView.alpha = 0.0
+            self.fadedView.isHidden = false
+            UIView.animate(withDuration: 1.0) {
+                self.fadedView.alpha = 1.0
+            }
+        }
+    }
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            audioEngine.inputNode.removeTap(onBus: 0)
+            self.speechData.append(Speech(Title: "New Recording", Date: Date(), Text: self.recordedMessage.text!))
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                self.fadedView.alpha = 0.0
+            }) { (finished) in
+                self.fadedView.isHidden = true
+                self.recordingView.isHidden = true
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func startRecording() {
+        
+        if let recognitionTask = self.recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        self.recordedMessage.text = ""
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation)
+        }catch {
+            print(error)
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let recognitionRequest = self.recognitionRequest else {
+            fatalError("Unable to create a speech audio buffer")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            if let result = result {
+                let sentence = result.bestTranscription.formattedString
+                self.recordedMessage.text = sentence
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                self.audioEngine.inputNode.removeTap(onBus: 0)
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                self.recordButton.isEnabled = true
+            }
+            
+        })
+        
+        let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do{
+            try audioEngine.start()
+        }catch {
+            print(error)
+        }
         
     }
 
